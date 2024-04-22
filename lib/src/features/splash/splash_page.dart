a669ca239d6cf283d736e526db765a165ce5e1aa
Felipe Ferreira <feferreira.sf@gmail.com>
2024-04-22 18:34:01 -0300

    ✨ logo animation when page is loading

--- lib/src/features/splash/splash_page.dart
+++ lib/src/features/splash/splash_page.dart
@@ -1,22 +1,63 @@
-import 'package:asyncstate/asyncstate.dart';
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  double _scale = 10.0;
+  double _animationOpacityLogo = 0.0;
+
+  /// Essa escala vai aumentar o tamanho original da logo
+  double get _logoAnimationWidth => 100 * _scale;
+  double get _logoAnimationHeight => 120 * _scale;
+
+  /// Ao iniciar a tela, o logo aparece com uma animação
+  /// voltando para o tamanho original e a opacitade para 1
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _animationOpacityLogo = 1.0;
+        _scale = 1.0;
+      });
+    });
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('DW Barbershop'),
-      ),
-      body: ElevatedButton(
-        onPressed: () async {
-          await Future.delayed(
-            const Duration(seconds: 2),
-          ).asyncLoader();
-        },
-        child: const Text('Clique para continuar'),
+      backgroundColor: Colors.black,
+      body: DecoratedBox(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background_image_chair.jpg'),
+            fit: BoxFit.cover,
+            opacity: .2,
+          ),
+        ),
+        child: Center(
+          child: AnimatedOpacity(
+            duration: const Duration(seconds: 3),
+            curve: Curves.easeIn,
+            opacity: _animationOpacityLogo,
+            child: AnimatedContainer(
+              duration: const Duration(seconds: 3),
+              curve: Curves.linearToEaseOut,
+              width: _logoAnimationWidth,
+              height: _logoAnimationHeight,
+              child: Image.asset(
+                'assets/images/imgLogo.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
