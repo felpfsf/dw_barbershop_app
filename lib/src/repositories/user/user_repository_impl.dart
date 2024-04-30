50b541bb2f07796006f710b9649dcd998b9cbec9
Felipe Ferreira <feferreira.sf@gmail.com>
2024-04-30 16:35:49 -0300

    ✨ user repository

--- lib/src/repositories/user/user_repository_impl.dart
+++ lib/src/repositories/user/user_repository_impl.dart
@@ -3,8 +3,10 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:dw_barbershop/src/core/exceptions/auth_exception.dart';
+import 'package:dw_barbershop/src/core/exceptions/repository_exception.dart';
 import 'package:dw_barbershop/src/core/fp/either.dart';
 import 'package:dw_barbershop/src/core/restClient/rest_client.dart';
+import 'package:dw_barbershop/src/models/user_model.dart';
 
 import './user_repository.dart';
 
@@ -35,6 +37,25 @@ class UserRepositoryImpl implements UserRepository {
           Left(AuthUnauthorizedException()),
         _ => Left(AuthError(message: '❌ Erro ao realizar login')),
       };
+    }
+  }
+
+  @override
+  Future<Either<RepositoryException, UserModel>> me() async {
+    try {
+      final Response(:data) = await restClient.auth.get('path');
+
+      return Right(UserModel.fromMap(data));
+    } on DioException catch (e, s) {
+      log('Erro ao buscar usuário', error: e, stackTrace: s);
+      return Left(
+        RepositoryException(message: '❌ Erro ao buscar usuário logado'),
+      );
+    } on ArgumentError catch (e, s) {
+      log('JSON invárlido', error: e, stackTrace: s);
+      return Left(
+        RepositoryException(message: e.message),
+      );
     }
   }
 }
