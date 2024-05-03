b37f8a7e19e5fab444ecfe1f444ff4db581e92c8
Felipe Ferreira <feferreira.sf@gmail.com>
2024-05-03 18:39:13 -0300

    🚧 placeholder route to register barbershop page

--- lib/src/barbershop_app.dart
+++ lib/src/barbershop_app.dart
@@ -3,6 +3,7 @@ import 'package:dw_barbershop/src/core/ui/barbershop_nav_global_key.dart';
 import 'package:dw_barbershop/src/core/ui/barbershop_theme.dart';
 import 'package:dw_barbershop/src/core/ui/widgets/barbershop_loader.dart';
 import 'package:dw_barbershop/src/features/auth/login/login_page.dart';
+import 'package:dw_barbershop/src/features/register/user_register_page.dart';
 import 'package:dw_barbershop/src/features/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -22,6 +23,10 @@ class BarbershopApp extends StatelessWidget {
           routes: {
             '/': (_) => const SplashPage(),
             '/auth/login': (_) => const LoginPage(),
+            '/auth/register/user': (_) => const UserRegisterPage(),
+            '/auth/register/barbershop': (_) => const Center(
+                  child: Text('Cadastrar estabelecimento'),
+                ),
             '/home/adm': (_) => const Center(
                   child: Text('ADM'),
                 ),
