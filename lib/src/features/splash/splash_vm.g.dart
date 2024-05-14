d6fa294c04065dfb58d9effa42bb1e1971e180e7
Felipe Ferreira <feferreira.sf@gmail.com>
2024-05-14 18:45:09 -0300

    ♻️ refactor splash page to get login state

---
+++ lib/src/features/splash/splash_vm.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'splash_vm.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$splashVmHash() => r'20a4f4eeb3e20551c0195d930e81bd0de74dbee5';
+
+/// See also [SplashVm].
+@ProviderFor(SplashVm)
+final splashVmProvider =
+    AutoDisposeAsyncNotifierProvider<SplashVm, SplashState>.internal(
+  SplashVm.new,
+  name: r'splashVmProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$splashVmHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SplashVm = AutoDisposeAsyncNotifier<SplashState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
