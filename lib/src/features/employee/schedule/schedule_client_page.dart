a09cbf17af60476b4077a558c80608b06d04fd61
Felipe Ferreira <feferreira.sf@gmail.com>
2024-05-23 19:14:43 -0300

    ✨ client schedule page

--- lib/src/features/employee/schedule/schedule_client_page.dart
+++ lib/src/features/employee/schedule/schedule_client_page.dart
@@ -1,13 +1,34 @@
 import 'package:dw_barbershop/src/core/ui/barbershop_icons.dart';
 import 'package:dw_barbershop/src/core/ui/barbershop_theme.dart';
 import 'package:dw_barbershop/src/core/ui/helpers/helper_form.dart';
+import 'package:dw_barbershop/src/core/ui/helpers/messages.dart';
 import 'package:dw_barbershop/src/core/ui/widgets/barbershop_hours_grid.dart';
 import 'package:dw_barbershop/src/core/ui/widgets/barbershop_user_avatar.dart';
 import 'package:dw_barbershop/src/features/home/widgets/schedule_calendar.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:validatorless/validatorless.dart';
 
-class ScheduleClientPage extends StatelessWidget {
+class ScheduleClientPage extends StatefulWidget {
   const ScheduleClientPage({super.key});
+
+  @override
+  State<ScheduleClientPage> createState() => _ScheduleClientPageState();
+}
+
+class _ScheduleClientPageState extends State<ScheduleClientPage> {
+  var dateFormat = DateFormat('dd/MM/yyyy');
+  bool showCalendar = false;
+  final formKey = GlobalKey<FormState>();
+  final clientEC = TextEditingController();
+  final dateSelectedEC = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    clientEC.dispose();
+    dateSelectedEC.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,42 +46,88 @@ class ScheduleClientPage extends StatelessWidget {
             const SizedBox(height: 24),
             const Text('Nome do colaborador'),
             const SizedBox(height: 36),
-            TextFormField(
-              onTapOutside: (_) => context.unfocus(),
-              decoration: const InputDecoration(
-                label: Text('Nome do cliente'),
-                hintText: 'Nome do cliente',
+            Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: clientEC,
+                    validator: Validatorless.multiple([
+                      Validatorless.required('Cliente obrigatório'),
+                      Validatorless.min(1, 'Nome do cliente muito curto'),
+                    ]),
+                    onTapOutside: (_) => context.unfocus(),
+                    decoration: const InputDecoration(
+                      label: Text('Nome do cliente'),
+                      hintText: 'Nome do cliente',
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+                  TextFormField(
+                    controller: dateSelectedEC,
+                    validator: Validatorless.required(
+                        'Selecione a data do agendamento'),
+                    onTap: () {
+                      setState(() {
+                        showCalendar = true;
+                      });
+                      context.unfocus();
+                    },
+                    readOnly: true,
+                    decoration: const InputDecoration(
+                      label: Text('Selecione uma data'),
+                      hintText: 'Selecione uma data',
+                      suffixIcon: Icon(
+                        BarbershopIcons.calendar,
+                        color: ColorsTheme.brown,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      const SizedBox(height: 24),
+                      Offstage(
+                        offstage: !showCalendar,
+                        child: ScheduleCalendar(
+                          cancelPressed: () {
+                            setState(() {
+                              showCalendar = false;
+                            });
+                          },
+                          okPressed: (DateTime value) {
+                            setState(() {
+                              dateSelectedEC.text = dateFormat.format(value);
+                              showCalendar = false;
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 24),
+                  BarbershopHoursGrid.singleSelection(
+                    startTime: 6,
+                    endTime: 22,
+                    enabledHours: const [6, 7, 8],
+                    onHourPressed: (value) {},
+                  ),
+                  const SizedBox(height: 24),
+                  ElevatedButton(
+                    onPressed: () {
+                      switch (formKey.currentState?.validate()) {
+                        case null || false:
+                          Messages.showError('Dados incompletos', context);
+                        case true:
+                        // API
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(56),
+                    ),
+                    child: const Text('AGENDAR'),
+                  )
+                ],
               ),
             ),
-            const SizedBox(height: 32),
-            TextFormField(
-              readOnly: true,
-              decoration: InputDecoration(
-                label: const Text('Selecione uma data'),
-                hintText: 'Selecione uma data',
-                suffixIcon: IconButton(
-                  icon: const Icon(BarbershopIcons.calendar),
-                  color: ColorsTheme.brown,
-                  onPressed: () {},
-                ),
-              ),
-            ),
-            const SizedBox(height: 24),
-            const ScheduleCalendar(),
-            const SizedBox(height: 24),
-            BarbershopHoursGrid(
-              startTime: 6,
-              endTime: 22,
-              onHourPressed: (value) {},
-            ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size.fromHeight(56),
-              ),
-              child: const Text('AGENDAR'),
-            )
           ],
         ),
       ),
--- lib/src/features/home/widgets/schedule_calendar.dart
+++ lib/src/features/home/widgets/schedule_calendar.dart
@@ -1,54 +1,99 @@
 import 'package:dw_barbershop/src/core/ui/barbershop_theme.dart';
+import 'package:dw_barbershop/src/core/ui/helpers/messages.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class ScheduleCalendar extends StatefulWidget {
-  const ScheduleCalendar({super.key});
+  final VoidCallback cancelPressed;
+  final ValueChanged<DateTime> okPressed;
+
+  const ScheduleCalendar({
+    super.key,
+    required this.cancelPressed,
+    required this.okPressed,
+  });
 
   @override
   State<ScheduleCalendar> createState() => _ScheduleCalendarState();
 }
 
 class _ScheduleCalendarState extends State<ScheduleCalendar> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
   @override
   Widget build(BuildContext context) {
-    DateTime? daySelected;
-
-    return Column(
-      children: [
-        TableCalendar(
-          headerStyle: const HeaderStyle(titleCentered: true),
-          focusedDay: DateTime.now(),
-          firstDay: DateTime.utc(2024, 01, 01),
-          lastDay: DateTime.now().add(const Duration(days: 365 * 10)),
-          calendarFormat: CalendarFormat.month,
-          locale: 'pt_BR',
-          availableGestures: AvailableGestures.horizontalSwipe,
-          availableCalendarFormats: const {
-            CalendarFormat.month: 'Month',
-          },
-          selectedDayPredicate: (day) {
-            return isSameDay(daySelected, day);
-          },
-          onDaySelected: (selectedDay, focusedDay) {
-            if (!isSameDay(daySelected, selectedDay)) {
-              setState(() {
-                daySelected = selectedDay;
-              });
-            }
-          },
-          calendarStyle: CalendarStyle(
-            selectedDecoration: const BoxDecoration(
-              color: ColorsTheme.brown,
-              shape: BoxShape.circle,
-            ),
-            todayDecoration: BoxDecoration(
-              color: ColorsTheme.brown.withOpacity(0.4),
-              shape: BoxShape.circle,
+    return Container(
+      decoration: BoxDecoration(
+        color: ColorsTheme.greyLight,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        children: [
+          TableCalendar(
+            headerStyle: const HeaderStyle(titleCentered: true),
+            focusedDay: _focusedDay,
+            firstDay: DateTime.utc(2024, 01, 01),
+            lastDay: DateTime.now().add(const Duration(days: 365 * 10)),
+            calendarFormat: CalendarFormat.month,
+            locale: 'pt_BR',
+            availableGestures: AvailableGestures.horizontalSwipe,
+            availableCalendarFormats: const {
+              CalendarFormat.month: 'Month',
+            },
+            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+            onDaySelected: (selectedDay, focusedDay) {
+              if (!isSameDay(_selectedDay, selectedDay)) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                });
+              }
+            },
+            calendarStyle: CalendarStyle(
+              selectedDecoration: const BoxDecoration(
+                color: ColorsTheme.brown,
+                shape: BoxShape.circle,
+              ),
+              todayDecoration: BoxDecoration(
+                color: ColorsTheme.brown.withOpacity(0.4),
+                shape: BoxShape.circle,
+              ),
             ),
           ),
-        ),
-      ],
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () {
+                  widget.cancelPressed();
+                },
+                child: Text(
+                  'Cancelar',
+                  style: BarbershopTheme.mediumBodyStyle.copyWith(
+                    color: ColorsTheme.brown,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  if (_selectedDay == null) {
+                    Messages.showError('Por favor escolha uma data', context);
+                    return;
+                  }
+                  widget.okPressed(_selectedDay!);
+                },
+                child: Text(
+                  'OK',
+                  style: BarbershopTheme.mediumBodyStyle.copyWith(
+                    color: ColorsTheme.brown,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
